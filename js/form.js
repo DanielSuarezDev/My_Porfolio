9d6e7c3be46ac444ee87fea44cd86083779d3e57
Daniel Suarez Dev <danielsuarez3114@gmail.com>
2020-11-27 15:46:29 -0500

    mejorando la navegabilidad

--- js/form.js
+++ js/form.js
@@ -14,13 +14,13 @@ document.querySelector("#submit").addEventListener("click", e => {
   resp.classList.remove("send");
 
   let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
-		*_MI NEGOCIO_*%0A
-		*Reservas*%0A%0A
-		*¿Cuál es tu nombre?*%0A
+		*_DANIEL SUAREZ DEV_*%0A
+		*Frontend Developer Freelancer*%0A%0A
+		*Hola me llamo...*%0A
 		${nombre}%0A
-		*Indica la hora de tu reserva*%0A
+		*Email:*%0A
 		${correo}%0A
-		*Empleado de preferencia*%0A
+		*Comentario:*%0A
 		${comentario}`;
 
   if (nombre === "" || correo === "" || comentario === "") {
