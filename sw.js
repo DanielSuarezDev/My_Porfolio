51ac3f45f4265f6b29e765054ae2a916ebd4f4cd
Daniel Suarez Dev <danielsuarez3114@gmail.com>
2020-12-02 13:38:47 -0500

    sw

---
+++ sw.js
@@ -0,0 +1,46 @@
+const VERSION = 'v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(precache());
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  // get
+  if (request.method !== 'GET') {
+    return;
+  }
+
+  // buscar en cache
+  event.respondWith(cachedResponse(request));
+
+  // actualizar el cache
+  event.waitUntil(updateCache(request));
+});
+
+async function precache() {
+  const cache = await caches.open(VERSION);
+  return cache.addAll([
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/form.js',
+    '/js/particles.js',
+    '/js/particulas.js',
+    '/css/mobilel.css',
+    '/css/tablet.css',
+    '/css/deskopt.css',
+  ]);
+}
+
+async function cachedResponse(request) {
+  const cache = await caches.open(VERSION);
+  const response = await cache.match(request);
+  return response || fetch(request);
+}
+
+async function updateCache(request) {
+  const cache = await caches.open(VERSION);
+  const response = await fetch(request);
+  return cache.put(request, response);
+}
